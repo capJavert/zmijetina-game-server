48460fbb810468096874ccb78d089cb65cbc755e
capJavert <ante.baric3@gmail.com>
2016-06-02 15:39:43 -0400

    server s osnovnim eventima, conn i disconn

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require('http'),
+    fs = require('fs'),
+    // NEVER use a Sync function except at start-up!
+    index = fs.readFileSync(__dirname + '/index.html');
 
-app.get('/update', function(req, res){
-  res.send("it works! :3000");
+// Send index.html to all requests
+var app = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(index);
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+var io = require('socket.io').listen(app);
+
+// Send current time to all connected clients
+function sendTime() {
+    io.emit('time', { time: new Date().toJSON() });
+}
+
+// Send current time every 10 secs
+//setInterval(sendTime, 5000);
+
+io.on('connection', function(socket) {
+    console.log("Client connected");
+    socket.emit("hello", { id: socket.id });
+    //socket.emit("answer");
+
+    socket.on("position", function(data) {
+        console.log(data);   
+    });
+
+    socket.on("disconnect", function() {
+        console.log("Client disconnected");
+    });
 });
+
+app.listen(3000);
+console.log("Listening on port 3000");
